245e124646e1a92d10185e2b870a8b4a0c223535
Andrea Sánchez Tapia <katori@gmail.com>
2019-08-16 13:18:39 -0700

    ajeitando o workflow

--- 0_filtrar_inpa.R
+++ 0_filtrar_inpa.R
@@ -1,4 +1,4 @@
-#filtrar o arquivo do inpa
+#filtrar o arquivo do inpa para usar o output no script - não rodar mais
 library(readxl)
 tabela_inpa_splink <- read_excel(path = "planilha_geral_splink_inpa.xlsx", sheet = 1)
 head(tabela_inpa_splink)
@@ -7,4 +7,4 @@ library(dplyr)
 names(tabela_inpa_splink)
 
 inpa_filtered <- tabela_inpa_splink %>% filter(tabela_inpa_splink$scientificname %in% especies)
-write.csv(inpa_filtered, "inpa_filtrado.csv")
+write.csv(inpa_filtered, "./data/inpa_filtrado.csv")
